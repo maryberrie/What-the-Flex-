41f0c3e8872923a6702bbfca31b13f67b538d12c
JustinKwon <justinkwon99@gmail.com>
2017-07-24 11:41:29 -0700

    basic python set up

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 
 env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'))
+
+class Page(webapp2.RequestHandler):
+    def get(self):
+
+        template = env.get_template('page.html')
+        self.response.out.write('hello')
+
+
+
+
+app = webapp2.WSGIApplication([
+        ('/', Page),
+], debug = True
+)
