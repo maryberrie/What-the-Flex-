9c52db8439e942a70db254e518882d15c3da2845
JustinKwon <justinkwon99@gmail.com>
2017-07-25 09:41:03 -0700

    name hange for the actual data collection page

--- main.py
+++ main.py
@@ -56,9 +56,9 @@ class Data(webapp2.RequestHandler):
         }
         template = env.get_template('data.html')
 
-        self.redirect('/data')
+        self.redirect('/')
 
-class Page(webapp2.RequestHandler):
+class InformationPage(webapp2.RequestHandler):
     def get(self):
 
 
@@ -67,6 +67,6 @@ class Page(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-        ('/', Page),
+        ('/', InformationPage),
         ('/data', Data)
 ], debug = True)
